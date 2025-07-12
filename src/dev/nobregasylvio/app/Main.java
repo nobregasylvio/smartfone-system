0173aed665145179339d5b6aadc9d9066d3a2bc7
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2025-07-12 01:16:23 -0300

    feat: adicionando metodos de menú

--- src/dev/nobregasylvio/app/Main.java
+++ src/dev/nobregasylvio/app/Main.java
@@ -1,4 +1,44 @@
 package dev.nobregasylvio.app;
 
+import dev.nobregasylvio.app.entities.Smartphone;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Smartphone smartphone = new Smartphone();
+
+        int option = 0;
+
+
+
+        scan.close();
+    }
+
+    public void menuPrincipal() {
+        System.out.println("----- SIMULADOR SMARTPHONE -----");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1: Realizar ligação");
+        System.out.println("2: Atender ligação");
+        System.out.println("3: Ouvir correio de voz");
+        System.out.println("4: Abrir Navegador de internet");
+        System.out.println("5: Abrir Reprodutor musical");
+    }
+
+    public void menuNavegadorInternet() {
+        System.out.println("----- INTERNET -----");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1: Exibir Página");
+        System.out.println("2: Criar nova aba");
+        System.out.println("3: Atualizar página");
+    }
+
+    public void menuReprodutorMusical() {
+        System.out.println("----- REPRODUTOR MUSICAL -----");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1: Tocar");
+        System.out.println("2: Pausar");
+        System.out.println("3: Selecionar música");
+    }
 }
